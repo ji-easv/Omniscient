c83057023406e7451621c052033b0f8bdfa106f5
Madsp15 <madsp@hotmail.dk>
2025-03-05 14:12:35 +0100

    Added Display with pagination and preview

---
+++ Omniscient.Web/Models/Email.cs
@@ -0,0 +1,8 @@
+﻿namespace Omniscient.Web.Models;
+
+public class Email
+{
+    public Guid EmailId { get; set; }
+    public required string FileName { get; set; }
+    public required string FullContent { get; set; }
+}
