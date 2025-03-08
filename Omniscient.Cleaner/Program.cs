46a6269f76fe2e1dd87d1bb2ff349af0dee5b209
MatejMa2ur <matejmazur@outlook.com>
2025-03-08 23:09:58 +0100

    feat: add file system repository for file processing
    • Implement `FileSystemRepository` with concurrency support for reading files.
    • Introduce `IFileSystemRepository` interface for dependency injection.
    • Add transient service registration for `IFileSystemRepository`.
    • Support initialization logic to fetch files from a specified path.
    • Update `.gitignore` and solution file to include new files.
    • Change default logging level to Debug for detailed logging.
    • Remove unused HTTP client file.

--- Omniscient.Cleaner/Program.cs
+++ Omniscient.Cleaner/Program.cs
@@ -5,6 +5,9 @@ using Omniscient.RabbitMQClient.Messages;
 using Omniscient.ServiceDefaults;
 using Omniscient.Shared.Entities;
 
+using Omniscient.Cleaner.Infrastructure;
+using Omniscient.Cleaner.Infrastructure.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,6 +17,7 @@ builder.Services.AddOpenApi();
 builder.Services.AddRabbitMqDependencies();
 
 builder.AddServiceDefaults();
+builder.Services.AddTransient<IFileSystemRepository, FileSystemRepository>();
 
 var app = builder.Build();
 
@@ -41,6 +45,22 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+if (args.Contains("init") || args.Contains("--init"))
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var fileSystemRepository = scope.ServiceProvider.GetService<IFileSystemRepository>();
+
+        if (fileSystemRepository != null)
+        {
+            var fileContent = await fileSystemRepository.GetFiles(
+                "/Users/mazur/Projects/School/PBSf25/1stSemester/DevelopmentOfLargeSystems/Week10-11/Omniscient/.enron-files/maildir");
+        }
+    }
+}
+
+
 
 app.UseHttpsRedirection();
 
