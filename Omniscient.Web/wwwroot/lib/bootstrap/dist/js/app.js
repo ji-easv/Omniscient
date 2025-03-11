4cf45fe9f6728843b91cb370e1541a6cc646ab7d
Julia Ilasova <1julka1il@gmail.com>
2025-03-11 11:44:36 +0100

    download functionality

---
+++ Omniscient.Web/wwwroot/lib/bootstrap/dist/js/app.js
@@ -0,0 +1,11 @@
+﻿window.downloadFileFromText = function (fileName, text) {
+    const blob = new Blob([text], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+};
