4c35f3fe25e3ed9614f1a1a29795694bebdb210b
MatejMa2ur <matejmazur@outlook.com>
2025-03-16 00:10:13 +0100

    feat: implement sharding and optimize file/email processing
    - Introduced sender-based sharding for email processing.
    - Updated Docker Compose for PostgreSQL shard configuration and indexer service separation.
    - Improved file processing logic with sender-specific batching.
    - Added support for message filtering in Indexer by sender range (A-M, N-Z).
    - Split large SQL inserts into smaller batches to avoid parameter limits.
    - Adjusted logging levels and added NackWithRequeue handling in RabbitMQ.

--- Omniscient.Indexer/EmailMessageHandler.cs
+++ Omniscient.Indexer/EmailMessageHandler.cs
@@ -3,7 +3,6 @@ using Omniscient.Indexer.Domain.Services;
 using Omniscient.RabbitMQClient.Interfaces;
 using Omniscient.RabbitMQClient.Messages;
 using Omniscient.ServiceDefaults;
-using Serilog;
 
 namespace Omniscient.Indexer;
 
@@ -11,11 +10,27 @@ public class EmailMessageHandler(IServiceProvider serviceProvider) : IRabbitMqMe
 {
     public async Task HandleMessageAsync(EmailMessage message, CancellationToken token = default)
     {
+        ShouldProcessMessage(message);
+
         using var activity = ActivitySources.OmniscientActivitySource.StartActivity(ActivityKind.Consumer, message.ActivityContext);
         using var scope = serviceProvider.CreateScope();
         
         var indexerService = scope.ServiceProvider.GetRequiredService<IIndexerService>();
-        Log.Information("Received email message: {@Message}", message);
+        //Log.Information("Received email message: {@Message}", message);
         await indexerService.IndexEmails(message.Emails);
     }
+    
+    private bool ShouldProcessMessage(EmailMessage message)
+    {
+        if (string.IsNullOrEmpty(message.Sender))
+            throw new Exception("Do not process message!");
+
+        char firstChar = char.ToUpper(message.Sender[0]);
+        
+        if(firstChar >= 'A' && firstChar <= 'M')
+            return true;
+
+        throw new Exception("Do not process message!");
+    }
+
 }
